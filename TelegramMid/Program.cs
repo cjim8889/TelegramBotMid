a583b4ccf4e0df40b6f8746a0774b7d8573b0cc0
Wuhao Chen <c.jim8889@outlook.com>
2019-04-18 22:48:28 +0100

    Initial commit

---
+++ TelegramMid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramMid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
